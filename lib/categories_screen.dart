191e9b3f38886566e2597d9a1f0e1445b0a9344e
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-04 22:02:04 +0530

    updated

---
+++ lib/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:meal_app/category_item.dart';
+import 'package:meal_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("DeliMeals"),),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: DUMMY_CATEGORIES.map((e) => CategoryItem(e.title, e.color)).toList(),
+      ),
+    );
+  }
+}
