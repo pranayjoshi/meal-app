f2051855628651267a295d1879b65f671f9ca5a1
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 07:30:21 +0530

    added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,14 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
 
 class MealItem extends StatelessWidget {
-
   final String title;
   final String imageUrl;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
 
-  MealItem({required this.title,required this.imageUrl,required this.duration,required this.complexity,required this.affordability});
+  MealItem(
+      {required this.title,
+      required this.imageUrl,
+      required this.duration,
+      required this.complexity,
+      required this.affordability});
 
   void SelectMeal() {}
 
@@ -27,7 +31,27 @@ class MealItem extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(15), topRight: Radius.circular(15)),
-            child: Image.network(imageUrl, height: 250, width: double.infinity, fit: BoxFit.cover,),
+            child: Image.network(
+              imageUrl,
+              height: 250,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 10,
+            child: Container(
+              width: 220,
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              color: Colors.black54,
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 26, color: Colors.white),
+                softWrap: true,
+                overflow: TextOverflow.fade,
+              ),
+            ),
           )
         ]),
       ),
