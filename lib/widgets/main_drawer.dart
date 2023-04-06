72f0a7cd602ef9032a2ea94c3628094109069958
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 23:21:45 +0530

    added

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -3,9 +3,18 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Drawer(
+      child: Column(children: <Widget>[
+        Container(
+          height: 120,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          child: Text("Cooking Up!", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30, color: Theme.of(context).primaryColor),),
+        )
+      ],),
+    );
   }
 }
