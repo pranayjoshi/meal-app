4924cc15be64947ce688d472cb7e3d3266a69fc1
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 18:43:28 +0530

    added

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:meal_app/dummy_data.dart';
+import 'package:meal_app/widgets/meal_item.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = "/meal-detail";
@@ -8,10 +10,21 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments;
+
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => mealId == meal.id);
+
     return Scaffold(
         appBar: AppBar(
           title: Text("$mealId"),
         ),
-        body:Center(child: Text("This Meal - $mealId!"),));
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover,),
+            )
+          ],
+        ));
   }
 }
