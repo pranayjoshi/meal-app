250a3a977f008d600ee8a9fb3c2a07ba91279948
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 23:34:22 +0530

    added

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -4,20 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
-
-
-  Widget buildListTile(String title, IconData icon, Function tapHandler){
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
-            leading: Icon(
-              icon,
-              size: 26,
-            ),
-            title: Text(
-              title,
-              style: TextStyle(fontFamily: "RobotoCondensed", fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            onTap: tapHandler(),
-          );
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: "RobotoCondensed",
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: tapHandler(),
+    );
   }
 
   @override
@@ -42,8 +43,12 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          buildListTile("Meals",Icons.restaurant, () {Navigator.of(context).pushNamed("/");}),
-          buildListTile("Filters",Icons.settings, () {Navigator.of(context).pushNamed(FiltersScreen.routeName);})
+          buildListTile("Meals", Icons.restaurant, () {
+            Navigator.of(context).pushNamed("/");
+          }),
+          buildListTile("Filters", Icons.settings, () {
+            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+          })
         ],
       ),
     );
