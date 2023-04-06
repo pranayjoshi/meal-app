aba7c820c55caf6cb32cd6c1c67082976352abd3
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 23:31:20 +0530

    added

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class MainDrawer extends StatelessWidget {
 
 
-  Widget buildListTile(String title, IconData icon){
+  Widget buildListTile(String title, IconData icon, Function tapHandler){
     return ListTile(
             leading: Icon(
               icon,
@@ -15,7 +15,7 @@ class MainDrawer extends StatelessWidget {
               title,
               style: TextStyle(fontFamily: "RobotoCondensed", fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            onTap: () {},
+            onTap: tapHandler(),
           );
   }
 
@@ -41,8 +41,8 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          buildListTile("Meals",Icons.restaurant),
-          buildListTile("Filters",Icons.settings)
+          buildListTile("Meals",Icons.restaurant, () {Navigator.of(context).pushNamed("/");}),
+          buildListTile("Filters",Icons.settings, () {})
         ],
       ),
     );
