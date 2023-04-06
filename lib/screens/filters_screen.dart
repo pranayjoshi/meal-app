48e8f1d939a596ebb7edda95e0630415d34ef6b9
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 23:35:37 +0530

    added

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -3,10 +3,13 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class FiltersScreen extends StatelessWidget {
-
   static const routeName = "/filters";
   @override
   Widget build(BuildContext context) {
-    return Center( child:  Text("Filters!"),);
+    return Scaffold(
+      appBar: AppBar(title: Text("Filters")),
+        body: Center(
+      child: Text("Filters!"),
+    ));
   }
 }
