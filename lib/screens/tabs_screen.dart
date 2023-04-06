61029ffc0a52b6c1872e8903af45a6ab1c734255
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 21:48:46 +0530

    added

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TabsScreen extends StatefulWidget {
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
