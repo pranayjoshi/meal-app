a6661164d962ad403f6b0485f055c340f03d6b26
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-06 23:21:52 +0530

    added

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -6,15 +6,25 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: <Widget>[
-        Container(
-          height: 120,
-          width: double.infinity,
-          padding: EdgeInsets.all(20),
-          alignment: Alignment.centerLeft,
-          child: Text("Cooking Up!", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30, color: Theme.of(context).primaryColor),),
-        )
-      ],),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            
+            child: Text(
+              "Cooking Up!",
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
