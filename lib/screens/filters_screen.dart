a4b4523e90d23f8aa4783c7395b0ed1e4770a1ad
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-07 20:17:05 +0530

    updated

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -3,15 +3,53 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:meal_app/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = "/filters";
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _lactoseFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
+
+  Widget _buildSwitchListTitle(
+      String title, String subt, bool currentVal, Function updateVal) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentVal,
+      onChanged: updateVal as void Function(bool)?,
+      subtitle: Text(subt),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Filters")),
-      drawer: MainDrawer(),
-        body: Center(
-      child: Text("Filters!"),
-    ));
+        appBar: AppBar(title: Text("Filters")),
+        drawer: MainDrawer(),
+        body: Column(children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Adjust your meal selection.",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: <Widget>[
+              _buildSwitchListTitle("Gluteen Free",
+                  "Only includes gluteen free items.", _glutenFree, (val) {
+                setState(() {
+                  _glutenFree = val;
+                });
+              })
+            ],
+          ))
+        ]));
   }
 }
